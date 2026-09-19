1e4760231b90e4e91af0bc9fdc2d741f71de8587
sevenaction-prog <sevenaction@gmail.com>
2026-09-19 18:12:13 +0200

    Bump Astra Android version to 0.1.11

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sevenaction.astra"
         minSdk = 33
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.1.10"
+        versionCode = 12
+        versionName = "0.1.11"
         ndk { abiFilters += listOf("arm64-v8a") }
     }
 
